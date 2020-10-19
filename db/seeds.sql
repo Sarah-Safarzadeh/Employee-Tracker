19808f6a001fc85df6ec4571870915af950def45
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-18 23:11:12 -0500

    added employee values to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,14 @@ VALUES
 ('Lawyer', 150000, 3),
 ('IT Support', 95000, 2),
 ('Office Administrator', 45000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Sarah', 'Jones', 2, null),
+('Carina', 'Row', 2, null),
+('Stacy', 'Paris', 3, null),
+('Victor', 'Fiore', 1, null),
+('Landon', 'Looper', 4, null),
+('Raj', 'Mason', 1, null),
+('Gearson', 'Rolly', 4, null),
+('Fiona', 'Apple', 1, null);
