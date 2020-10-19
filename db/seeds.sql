0028da8ff585e6db7212562c5333dbccb8c71698
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-18 23:05:42 -0500

    value and role data in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES
+('Finance'),
+('Technology'),
+('Legal'),
+('Administration');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Accountant', 90000, 1),
+('CFO', 400000, 1),
+('Controller', 130000, 1),
+('Data Scientist', 80000, 2),
+('Software Engineer', 150000, 2),
+('Lawyer', 150000, 3),
+('IT Support', 95000, 2),
+('Office Administrator', 45000, 4);
--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 // DEPENDENCIES
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const consoleTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3001',
+    user: 'root',
+    password: 'sarah',
+    database: 'company'
+});
